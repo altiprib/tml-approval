bc00a6b1900765f2fef262d54ac701f83fa6e135
Ingo Pribik <ingo.pribik@flex.com>
2018-01-11 06:58:19 -0800

    [Extension] Create node "compaynies", Update node "compaynies", Create node "id", Create node "desc", Create screen "company"

---
+++ client/controllers/app/company.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_company', app_company);
+function app_company($scope, app) {
+    'use strict';
+    app.init($scope);
+}
---
+++ client/mocks/company.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.company = window.mocks.company || {};
